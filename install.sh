23a114210bef25ff501b4641ac7e6802b844051c
Alexey Baranov <a.baranov@office.ngs.ru>
2016-04-25 13:11:30 +0600

    install.sh updated

--- install.sh
+++ install.sh
@@ -3,49 +3,51 @@
 export dotfiles=~/.dotfiles
 
 function set_link {
-	if [ -e $dotfiles/$@ ]; then
-		from=$dotfiles/$@
-		to=/home/`whoami`/$@
-		command="ln -sf $from $to"
-		echo $command
-		exec 2>&1 $command &
-	fi
+    if [ -e $dotfiles/$@ ]; then
+        from=$dotfiles/$@
+        to=/home/`whoami`/$@
+        command="ln -sf $from $to"
+        echo $command
+        exec 2>&1 $command &
+    fi
 }
 
 function cleanup {
-	rm ~/.msmtprc
-	rm ~/.fonts
-	rm ~/.local/share/fonts
-	rm ~/.terminfo
-	rm ~/.i3
-	rm ~/.xkb
-	rm ~/.vim
-	rm ~/.mcabber
-	rm ~/bin
-	rm ~/.Xresources.colors
-	rm ~/.ardour-templates
-	rm ~/.jack-templates
-	rm ~/.guitarix
-	rm ~/.mutt
-	rm ~/.rack
-	rm ~/.icons
+    rm ~/.msmtprc
+    rm ~/.fonts
+    rm ~/.local/share/fonts
+    rm ~/.terminfo
+    rm ~/.i3
+    rm ~/.xkb
+    rm ~/.vim
+    rm ~/.mcabber
+    rm ~/bin
+    rm ~/.Xresources.colors
+    rm ~/.ardour-templates
+    rm ~/.jack-templates
+    rm ~/.guitarix
+    rm ~/.mutt
+    rm ~/.rack
+    rm ~/.icons
 }
 
 function install_i3_config {
-	cp `pwd`/.i3/config.default `pwd`/.i3/config
-	cat `pwd`/.i3/config.`hostname` >> `pwd`/.i3/config
+    cp `pwd`/.i3/config.default `pwd`/.i3/config
+    cat `pwd`/.i3/config.`hostname` >> `pwd`/.i3/config
 }
 
 function install {
-	export -f set_link
-	git ls-files | grep -E "^\.|^bin" | cut -f1 -d / | uniq | sort | xargs -n1 -I{} bash -c "set_link {}"
+    export -f set_link
+    git ls-files | grep -E "^\.|^bin" | cut -f1 -d / | uniq | sort | xargs -n1 -I{} bash -c "set_link {}"
 }
 
 function post_install {
-	ln -sf ~/.fonts ~/.local/share/fonts
-	fc-cache -f
-	localectl set-locale LANG=en_US.utf8
-	xsetup
+    ln -sf ~/.fonts ~/.local/share/fonts
+    fc-cache -f
+    localectl set-locale LANG=en_US.utf8
+    xsetup
+    screens-setup
+    sound-setup
 }
 
 cleanup
