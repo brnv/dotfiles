fff859dc08bc4a4a76f9b424cfd0af7f4d82717e
Alexey Baranov <a.baranov@office.ngs.ru>
2016-06-08 17:17:33 +0600

    root_install

--- root_install.sh
+++ root_install.sh
@@ -2,19 +2,29 @@
 
 set -euo pipefail
 
+:usage() {
+    echo "root_install [-h] [-r]"
+}
+
 dotfiles=~/.dotfiles
 hostname=`hostname`
 dryrun=false
 
-while getopts "r" key; do
+while getopts "rh" key; do
     case "$key" in
         r)
             dryrun=true
             ;;
+        h)
+            :usage
+            exit 0
+            ;;
     esac
+
+    shift
 done
 
-git-ls-files() {
+:git-ls-files() {
     local dir=$1
     local gitdir=${2:-$dotfiles}
     local files=$(git --git-dir=$gitdir/.git ls-files "$dir")
@@ -22,15 +32,15 @@ git-ls-files() {
     for file in $files; do
         file=$gitdir/$file
 
-        if [ $(is-git-submodule $file) ]; then
-            git-ls-files "" $file
+        if [ $(:is-git-submodule $file) ]; then
+            :git-ls-files "" $file
         else
             echo $file
         fi
     done
 }
 
-is-git-submodule() {
+:is-git-submodule() {
     local dir=$1
 
     if [ -d $dir ] && [ -f $dir/.git ]; then
@@ -40,25 +50,26 @@ is-git-submodule() {
     fi
 }
 
-remove-rootfs-prefix() {
+:remove-rootfs-prefix() {
     local file=$1
 	sed 's/.*rootfs//' <<< $file
 }
 
-remove-hostname() {
+:remove-hostname() {
     local file=$1
 	sed -e "s/\.home$//" -e "s/\.work$//" <<< $file
 }
 
-format-root-file() {
+:format-root-file() {
     local file=$1
 
-    file=$(remove-rootfs-prefix $file)
-    file=$(remove-hostname $file)
+    file=$(:remove-rootfs-prefix $file)
+    file=$(:remove-hostname $file)
+
     echo $file
 }
 
-should-copy() {
+:should-copy-file() {
     local file=$1
 
     if [[ $file != *work ]] && [[ $file != *home ]]; then
@@ -70,9 +81,9 @@ should-copy() {
     fi
 }
 
-copy() {
+:copy-file() {
 	local source=$1
-	local dest=$(format-root-file $source)
+	local dest=$(:format-root-file $source)
 
     echo sudo cp -ruT $source $dest
 
@@ -81,10 +92,10 @@ copy() {
     fi
 }
 
-should-remove() {
+:should-remove-file() {
     local file=$1
 
-    if [[ -e $(format-root-file $file) ]]; then
+    if [[ -e $(:format-root-file $file) ]]; then
         echo 1
         exit
     fi
@@ -92,8 +103,8 @@ should-remove() {
     echo
 }
 
-remove() {
-    local file=$(format-root-file $1)
+:remove-file() {
+    local file=$(:format-root-file $1)
 
     echo sudo rm -rf $file
 
@@ -102,19 +113,19 @@ remove() {
     fi
 }
 
-install() {
-    for file in $(git-ls-files rootfs); do
-        if [ $(should-remove $file) ]; then
-            remove $file
+:main() {
+    if $dryrun; then
+        echo "dry run, list of commands to be executed"
+    fi
+
+    for file in $(:git-ls-files rootfs); do
+        if [ $(:should-remove-file $file) ]; then
+            :remove-file $file
         fi
-        if [ $(should-copy $file) ]; then
-            copy $file
+        if [ $(:should-copy-file $file) ]; then
+            :copy-file $file
         fi
     done
 }
 
-if $dryrun; then
-    echo list of commands to be executed
-fi
-
-install
+:main
