d2d75acd589e63f79475a5282c30eec87ef7f096
Alexey Baranov <a.baranov@office.ngs.ru>
2016-06-14 13:05:21 +0600

    zsh: ash aliases

---
+++ .zsh/aliases/ash.zsh
@@ -0,0 +1,12 @@
+function ash-action() {
+    local action=$1
+    local subject=$2
+
+    ash $subject $action
+}
+
+alias ai="ash inbox"
+alias al="ash-action ls"
+alias ar="ash-action review"
+alias aa="ash-action approve"
+alias apl="ash-project ls-reviews"
