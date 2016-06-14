7260b574c21e47d53f7fa6c6196e2ccc2ec47235
Alexey Baranov <a.baranov@office.ngs.ru>
2016-06-14 14:27:14 +0600

    zsh: alias fix

--- .zsh/zaliases.zsh
+++ .zsh/zaliases.zsh
@@ -78,7 +78,7 @@ alias -g W="| wc"
 # dotfiles editing
 alias v="vim ~/.vimrc"
 alias z="vim ~/.zshrc"
-alias za="vim ~/.zsh/zaliases"
+alias za="vim ~/.zsh/zaliases.zsh"
 alias vs="vim .vim/plugins/snippets/go.snippets"
 
 # zsh controls
