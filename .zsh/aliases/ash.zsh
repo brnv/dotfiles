8c362ab1e8a577293e49b5f3b9b341b8654dd217
Alexey Baranov <a.baranov@office.ngs.ru>
2016-08-12 14:17:25 +0700

    zsh: ash aliases

--- .zsh/aliases/ash.zsh
+++ .zsh/aliases/ash.zsh
@@ -10,3 +10,13 @@ alias al="ash-action ls"
 alias ar="ash-action review"
 alias aa="ash-action approve"
 alias alr="ash-action ls-reviews"
+
+function ash-action-project-pull-request() {
+    local project=$1
+    local action=$2
+    local pull_request=$3
+
+    ash $project/$pull_request $action
+}
+
+alias ard="ash-action-project-pull-request deployerd review"
